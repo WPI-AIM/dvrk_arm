956d680963ccf1b32b5acd153f6dd181fb75f08f
Adnan Munawar <adnan.munawar@live.com>
2018-08-13 18:47:47 -0400

    Added author information

--- src/Timing.cpp
+++ src/Timing.cpp
@@ -33,7 +33,7 @@
  *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  *  POSSIBILITY OF SUCH DAMAGE.
  *
- * Author: Eitan Marder-Eppstein
+ * Author: Eitan Marder-Eppstein, Adnan Munawar
  *********************************************************************/
 
 #include "dvrk_arm/Timing.h"
